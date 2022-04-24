38c6f1a8ece805912a39f8e7a5b557f37b9e64d8
leshafun <leshafun19@yandex.ru>
2022-04-24 14:46:17 +0700

    Второй раз

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,37 @@
 const popupButtonOpen = document.querySelector('.profile__edit-button');
+
 const popup = document.querySelector('.popup');
+
 const popupButtonClose = popup.querySelector('.popup__button-close');
-const popupSaveData = document.querySelector('.popup__button-save');
+
+const profileName = document.querySelector('.profile__name');
+
+const profileDescription = document.querySelector('.profile__description');
+
+const formEditProfile = document.querySelector('.popup__form');
+
+const popupUserName = formEditProfile.querySelector('#username');
+
+const popupDescription = formEditProfile.querySelector('#description');
 
 function OpenTogglePopup() {
-    popup.classList.toggle('popup__open');
+    popup.classList.toggle('popup_open');
+    if (popup.classList.contains('popup_open')) {
+        popupUserName.value = profileName.textContent;
+        popupDescription.value = profileDescription.textContent;
+    }
 }
 
-function popupOverlayClickHadnler(evt) {
-    if (evt.target === evt.currentTarget) {
-        OpenTogglePopup();
-    }
+function onSubmitHandler(e) {
+    e.preventDefault();
+    profileName.textContent = popupUserName.value;
+    profileDescription.textContent = popupDescription.value;
+    OpenTogglePopup();
 }
 
 popupButtonOpen.addEventListener('click', OpenTogglePopup);
 
 popupButtonClose.addEventListener('click', OpenTogglePopup);
 
-popup.addEventListener('click', popupOverlayClickHadnler);
-
-popupSaveData.addEventListener('click', OpenTogglePopup);
-
-
-
-
-
-const profileName = document.querySelector('.profile__name');
-const profileDescription = document.querySelector('.profile__description');
-const formEditProfile = document.querySelector('.popup__form');
-const popupUserName = formEditProfile.querySelector('#username');
-const popupDescription = formEditProfile.querySelector('#description');
-
-
-function setFormProfileName(event) {
-    popupUserName.value = profileName.textContent
-}
-
-setFormProfileName();
-
-function setFormProfileDescription(event) {
-    popupDescription.value = profileDescription.textContent
-}
-
-setFormProfileDescription();
-
-function onSubmitHandler(e) {
-    e.preventDefault();
-    profileName.textContent = popupUserName.value;
-    profileDescription.textContent = popupDescription.value;
-}
-
 formEditProfile.addEventListener('submit', onSubmitHandler);
+
