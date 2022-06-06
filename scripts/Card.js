002bf89ccb1902b1cbf8365414c8e913fbb7a41e
leshafun <leshafun19@yandex.ru>
2022-06-06 19:56:05 +0700

    ПР7-1

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,66 +1,65 @@
-
-import { openPopup } from './index.js';
+import { openPopup } from "./index.js";
 
 export class Card {
-    constructor(data, cardSelector) {
-        this._name = data.name;
-        this._link = data.link;
-        this._cardSelector = cardSelector;
-        this._element = this._getCardTemplate();
-        this._toggleLikeButton = this._element.querySelector('.element__like-button');
-        this._popupShowImage = document.querySelector('.popup_show-image');
-        this._popupImage = this._popupShowImage.querySelector('.popup__image');
-        this._popupImageTitle = this._popupShowImage.querySelector('.popup__image-title');
-        this._titleCard = this._element.querySelector('.element__title');
-        this._imageCard = this._element.querySelector('.element__image');
-        this._deleteCardButton = this._element.querySelector('.element__delete-button');
-    };
+  constructor(data, cardSelector) {
+    this._name = data.name;
+    this._link = data.link;
+    this._cardSelector = cardSelector;
+    this._element = this._getCardTemplate();
+    this._buttonLikeToggle = this._element.querySelector(
+      ".element__like-button"
+    );
+    this._popupShowImage = document.querySelector(".popup_show-image");
+    this._popupImage = this._popupShowImage.querySelector(".popup__image");
+    this._popupImageTitle = this._popupShowImage.querySelector(
+      ".popup__image-title"
+    );
+    this._titleCard = this._element.querySelector(".element__title");
+    this._imageCard = this._element.querySelector(".element__image");
+    this._deleteCardButton = this._element.querySelector(
+      ".element__delete-button"
+    );
+  }
 
-    _getCardTemplate() {
-        const cardElement = document
-            .querySelector(this._cardSelector)
-            .content
-            .querySelector('.element')
-            .cloneNode(true);
+  _getCardTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".element")
+      .cloneNode(true);
 
-        return cardElement;
-    };
+    return cardElement;
+  }
 
-    generateCard() {
-        this._setEventListeners();
-        this._imageCard.alt = this._name;
-        this._imageCard.src = this._link;
-        this._titleCard.textContent = this._name;
+  generateCard() {
+    this._setEventListeners();
+    this._imageCard.alt = this._name;
+    this._imageCard.src = this._link;
+    this._titleCard.textContent = this._name;
 
-        return this._element;
-    };
+    return this._element;
+  }
 
+  _deleteCardTemplate() {
+    this._element.remove();
+    this._element = null;
+  }
 
-    _deleteCardTemplate() {
-        this._element.closest('.element').remove();
-};
+  _likeButton() {
+    this._buttonLikeToggle.classList.toggle("element__like-button_active");
+  }
 
-    _likeButton() {
-        this._toggleLikeButton.classList.toggle('element__like-button_active');
-    };
+  _openPopupImage() {
+    this._popupImage.alt = this._name;
+    this._popupImage.src = this._link;
+    this._popupImageTitle.textContent = this._name;
+    openPopup(this._popupShowImage);
+  }
 
-    _openPopupImage() {
-        this._popupImage.alt = this._name;
-        this._popupImage.src = this._link;
-        this._popupImageTitle.textContent = this._name;
-        openPopup(this._popupShowImage);
-    };
-
-    _setEventListeners() {
-        this._imageCard.addEventListener('click', () => this._openPopupImage());
-        this._toggleLikeButton.addEventListener('click', () => this._likeButton());
-        this._deleteCardButton.addEventListener('click', () => this._deleteCardTemplate());
-    }
-
-};
-
-
-
-
-
-
+  _setEventListeners() {
+    this._imageCard.addEventListener("click", () => this._openPopupImage());
+    this._buttonLikeToggle.addEventListener("click", () => this._likeButton());
+    this._deleteCardButton.addEventListener("click", () =>
+      this._deleteCardTemplate()
+    );
+  }
+}
